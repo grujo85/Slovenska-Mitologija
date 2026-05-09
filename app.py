6e07f7cd412bf13c06fec70a80e1c1f15c2a2003
grujo85 <vladimirgrujovic85@gmail.com>
2026-05-09 21:20:25 +0200

    app.py

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ def uzmi_iz_baze(upit=""):
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     if upit:
-        # Sortiramo rezultate pretrage azbučno
         cursor.execute("SELECT * FROM bica WHERE ime LIKE ? ORDER BY ime", ('%' + upit + '%',))
     else:
-        # Sortiramo sva bića azbučno
         cursor.execute("SELECT * FROM bica ORDER BY ime")
     rezultati = cursor.fetchall()
     conn.close()
@@ -22,6 +20,17 @@ def galerija():
     pretraga = request.args.get('search', '')
     podaci = uzmi_iz_baze(pretraga)
     return render_template('galerija.html', bica=podaci)
+
+# --- NOVA KARTICA ZA BORBU ---
+@app.route('/arena')
+def arena():
+    conn = sqlite3.connect('mitologija.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM bica ORDER BY ime")
+    borci = cursor.fetchall()
+    conn.close()
+    return render_template('arena.html', borci=borci)
 
 if __name__ == '__main__':
     app.run(debug=True)
